61950078ac6e403e204535d884badec1f8c3e4f4
Fernando Gonzalez Ruano <fegor@fegor.com>
2025-01-23 23:21:49 +0100

    Working

--- lfs-build.sh
+++ lfs-build.sh
@@ -505,7 +505,8 @@ mkdir -pv build && cd build
     --enable-64-bit-bfd        \
     --enable-new-dtags         \
     --enable-default-hash-style=gnu
-make tooldir=/usr && make tooldir=/usr install
+make
+make DESTDIR=$LFS install
 rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
 EOF
 }
@@ -713,7 +714,7 @@ buildtools() {
     #download_sources
     #prepare_environment
     #build_temporary_tools
-    #notify_build_base
+    notify_build_base
     build_temporary_tools_chroot
 }
 
@@ -724,12 +725,99 @@ buildbase() {
     echo "Instalación completada. Revisa el log en $LOG_FILE para más detalles."
 }
 
+build_gettext() {
+    cd /sources
+    tar -xf gettext-0.22.5.tar.xz && cd gettext-0.22.5
+    ./configure --disable-shared
+    make
+    cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /usr/bin
+}
+
+build_bison() {
+    cd /sources
+    tar bison-3.8.2.tar.xz && cd bison-3.8.2
+    ./configure --prefix=/usr \
+            --docdir=/usr/share/doc/bison-3.8.2    
+    make && make install
+}
+
+build_perl() {
+    cd /sources
+    tar -xf perl-5.40.0.tar.xz && cd perl-5.40.0
+    sh Configure -des                                     \
+             -D prefix=/usr                               \
+             -D vendorprefix=/usr                         \
+             -D useshrplib                                \
+             -D privlib=/usr/lib/perl5/5.40/core_perl     \
+             -D archlib=/usr/lib/perl5/5.40/core_perl     \
+             -D sitelib=/usr/lib/perl5/5.40/site_perl     \
+             -D sitearch=/usr/lib/perl5/5.40/site_perl    \
+             -D vendorlib=/usr/lib/perl5/5.40/vendor_perl \
+             -D vendorarch=/usr/lib/perl5/5.40/vendor_perl
+    make && make install
+}
+
+build_python() {
+    cd /sources
+    tar -xf Python-3.12.5.tar.xz && cd Python-3.12.5
+    ./configure --prefix=/usr   \
+                --enable-shared \
+                --without-ensurepip
+    make && make install
+}
+
+build_texinfo() {
+    cd /sources
+    tar -xf texinfo-7.1.tar.xz && cd texinfo-7.1
+    ./configure --prefix=/usr
+    make && make install
+}
+
+build_util_linux() {
+    cd /sources
+    tar -xf util-linux-2.40.2.tar.xz && cd util-linux-2.40.2
+    mkdir -pv /var/lib/hwclock
+    ./configure --libdir=/usr/lib     \
+                --runstatedir=/run    \
+                --disable-chfn-chsh   \
+                --disable-login       \
+                --disable-nologin     \
+                --disable-su          \
+                --disable-setpriv     \
+                --disable-runuser     \
+                --disable-pylibmount  \
+                --disable-static      \
+                --disable-liblastlog2 \
+                --without-python      \
+                ADJTIME_PATH=/var/lib/hwclock/adjtime \
+                --docdir=/usr/share/doc/util-linux-2.40.2
+    make && make install
+}   
+
+cleaning_and_backup() {
+    rm -rf /usr/share/{info,man,doc}/*
+    find /usr/{lib,libexec} -name \*.la -delete
+    rm -rf /tools
+
+    #TODO: Backup de los archivos de configuración
+    #       parámetros: backup, restore y continue_after_backup
+}
+
 # Función para continuar con la construcción del sistema base
 buildbase_continue() {
     touch /var/log/{btmp,lastlog,faillog,wtmp}
     chgrp -v utmp /var/log/lastlog
     chmod -v 664  /var/log/lastlog
     chmod -v 600  /var/log/btmp
+
+    build_gettext
+    build_bison
+    build_perl
+    build_python
+    build_texinfo
+    build_util_linux
+    cleaning_and_backup
+
 }
 
 # Función principal
