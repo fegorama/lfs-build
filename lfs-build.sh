69351200668b453428e57ad70418bad2235ece2b
fegor <fegor@fegor.com>
2025-01-30 09:52:50 +0100

    Working...

--- lfs-build.sh
+++ lfs-build.sh
@@ -1944,86 +1944,355 @@ endif
 EOF
 }
 
+install_markupsafe() {
+    cd /sources
+    tar -xf MarkupSafe-2.1.5.tar.gz && cd MarkupSafe-2.1.5
+    pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
+    pip3 install --no-index --no-user --find-links dist Markupsafe
+}
+
+install_jinja2() {
+    cd /sources
+    tar -xf jinja2-3.1.4.tar.gz && cd jinja2-3.1.4
+    pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
+    pip3 install --no-index --no-user --find-links dist Jinja2
+}
+
+install_udev_from_systemd() {
+    cd /sources
+    tar -xf systemd-256.4.tar.gz && cd systemd-256.4
+    sed -i -e 's/GROUP="render"/GROUP="video"/' \
+       -e 's/GROUP="sgx", //' rules.d/50-udev-default.rules.in
+    sed '/systemd-sysctl/s/^/#/' -i rules.d/99-systemd.rules.in
+    sed '/NETWORK_DIRS/s/systemd/udev/' -i src/basic/path-lookup.h
+    mkdir -p build
+    cd       build
+
+    meson setup ..                  \
+        --prefix=/usr             \
+        --buildtype=release       \
+        -D mode=release           \
+        -D dev-kvm-mode=0660      \
+        -D link-udev-shared=false \
+        -D logind=false           \
+        -D vconsole=false
+    
+    export udev_helpers=$(grep "'name' :" ../src/udev/meson.build | \
+                      awk '{print $3}' | tr -d ",'" | grep -v 'udevadm')
+
+    ninja udevadm systemd-hwdb                                           \
+      $(ninja -n | grep -Eo '(src/(lib)?udev|rules.d|hwdb.d)/[^ ]*') \
+      $(realpath libudev.so --relative-to .)                         \
+      $udev_helpers
+    
+    install -vm755 -d {/usr/lib,/etc}/udev/{hwdb.d,rules.d,network}
+    install -vm755 -d /usr/{lib,share}/pkgconfig
+    install -vm755 udevadm                             /usr/bin/
+    install -vm755 systemd-hwdb                        /usr/bin/udev-hwdb
+    ln      -svfn  ../bin/udevadm                      /usr/sbin/udevd
+    cp      -av    libudev.so{,*[0-9]}                 /usr/lib/
+    install -vm644 ../src/libudev/libudev.h            /usr/include/
+    install -vm644 src/libudev/*.pc                    /usr/lib/pkgconfig/
+    install -vm644 src/udev/*.pc                       /usr/share/pkgconfig/
+    install -vm644 ../src/udev/udev.conf               /etc/udev/
+    install -vm644 rules.d/* ../rules.d/README         /usr/lib/udev/rules.d/
+    install -vm644 $(find ../rules.d/*.rules \
+                        -not -name '*power-switch*') /usr/lib/udev/rules.d/
+    install -vm644 hwdb.d/*  ../hwdb.d/{*.hwdb,README} /usr/lib/udev/hwdb.d/
+    install -vm755 $udev_helpers                       /usr/lib/udev
+    install -vm644 ../network/99-default.link          /usr/lib/udev/network
+
+    tar -xvf ../../udev-lfs-20230818.tar.xz
+    make -f udev-lfs-20230818/Makefile.lfs install
+
+    tar -xf ../../systemd-man-pages-256.4.tar.xz                            \
+    --no-same-owner --strip-components=1                              \
+    -C /usr/share/man --wildcards '*/udev*' '*/libudev*'              \
+                                  '*/systemd.link.5'                  \
+                                  '*/systemd-'{hwdb,udevd.service}.8
+
+    sed 's|systemd/network|udev/network|'                                 \
+        /usr/share/man/man5/systemd.link.5                                \
+    > /usr/share/man/man5/udev.link.5
+
+    sed 's/systemd\(\\\?-\)/udev\1/' /usr/share/man/man8/systemd-hwdb.8   \
+                                > /usr/share/man/man8/udev-hwdb.8
+
+    sed 's|lib.*udevd|sbin/udevd|'                                        \
+        /usr/share/man/man8/systemd-udevd.service.8                       \
+    > /usr/share/man/man8/udevd.8
+
+    rm /usr/share/man/man*/systemd*
+
+    unset udev_helpers
+    udev-hwdb update
+}
+
+install_mandb() {
+    cd /sources
+    tar -xf man-db-2.12.1.tar.xz && cd man-db-2.12.1
+    ./configure --prefix=/usr                         \
+                --docdir=/usr/share/doc/man-db-2.12.1 \
+                --sysconfdir=/etc                     \
+                --disable-setuid                      \
+                --enable-cache-owner=bin              \
+                --with-browser=/usr/bin/lynx          \
+                --with-vgrind=/usr/bin/vgrind         \
+                --with-grap=/usr/bin/grap             \
+                --with-systemdtmpfilesdir=            \
+                --with-systemdsystemunitdir=
+    make
+    make check
+    make install
+}
+
+install_procps_ng() {
+    cd /sources
+    tar -xf procps-ng-4.0.4.tar.xz && cd procps-ng-4.0.4
+    ./configure --prefix=/usr                           \
+                --docdir=/usr/share/doc/procps-ng-4.0.4 \
+                --disable-static                        \
+                --disable-kill
+    make
+    chown -R tester .
+    su tester -c "PATH=$PATH make check"
+    make install
+}
+
+install_util_linux() {
+    cd /sources/util-linux-2.40.2
+    ./configure --bindir=/usr/bin     \
+                --libdir=/usr/lib     \
+                --runstatedir=/run    \
+                --sbindir=/usr/sbin   \
+                --disable-chfn-chsh   \
+                --disable-login       \
+                --disable-nologin     \
+                --disable-su          \
+                --disable-setpriv     \
+                --disable-runuser     \
+                --disable-pylibmount  \
+                --disable-liblastlog2 \
+                --disable-static      \
+                --without-python      \
+                --without-systemd     \
+                --without-systemdsystemunitdir        \
+                ADJTIME_PATH=/var/lib/hwclock/adjtime \
+                --docdir=/usr/share/doc/util-linux-2.40.2
+    touch /etc/fstab
+    chown -R tester .
+    su tester -c "make -k check"
+    make install
+}
+
+install_e2fsprogs() {
+    cd /sources
+    tar -xf e2fsprogs-1.47.1.tar.gz && cd e2fsprogs-1.47.1
+    mkdir -v build
+    cd build
+    ../configure --prefix=/usr           \
+                --sysconfdir=/etc       \
+                --enable-elf-shlibs     \
+                --disable-libblkid      \
+                --disable-libuuid       \
+                --disable-uuidd         \
+                --disable-fsck
+    make clean && make
+    set +e
+    make check
+    make install
+    rm -fv /usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
+    set -e
+
+    gunzip -v /usr/share/info/libext2fs.info.gz
+    install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info
+
+    makeinfo -o      doc/com_err.info ../lib/et/com_err.texinfo
+    install -v -m644 doc/com_err.info /usr/share/info
+    install-info --dir-file=/usr/share/info/dir /usr/share/info/com_err.info
+
+    sed 's/metadata_csum_seed,//' -i /etc/mke2fs.conf
+}
+
+install_sysklogd() {
+    cd /sources
+    tar -xf sysklogd-2.6.1.tar.gz && cd sysklogd-2.6.1
+    ./configure --prefix=/usr      \
+            --sysconfdir=/etc  \
+            --runstatedir=/run \
+            --without-logger
+    make
+    make install
+
+    cat > /etc/syslog.conf << "EOF"
+# Begin /etc/syslog.conf
+
+auth,authpriv.* -/var/log/auth.log
+*.*;auth,authpriv.none -/var/log/sys.log
+daemon.* -/var/log/daemon.log
+kern.* -/var/log/kern.log
+mail.* -/var/log/mail.log
+user.* -/var/log/user.log
+*.emerg *
+
+# Do not open any internet ports.
+secure_mode 2
+
+# End /etc/syslog.conf
+EOF
+
+}
+
+install_sysvinit() {
+    cd /sources
+    tar -xf sysvinit-3.10.tar.xz && cd sysvinit-3.10
+    patch -Np1 -i ../sysvinit-3.10-consolidated-1.patch
+    make 
+    make install
+}
+
 installing_basic_system() {
-# TODO: descomentar en la versión final.
-    ##package_management
-    #install_man_pages
-    #install_iana_etc
-    #install_glibc
-    #install_zlib
-    #install_bzip
-    #install_xz
-    #install_lz4
-    #install_zstd
-    #install_file
-    #install_readline
-    #install_m4
-    #install_bc
-    #install_flex
-    #install_tcl
-    #install_spect
-    #install_dejaGNU
-    #install_pkgconf
-    #install_binutils
-    #install_gmp
-    #install_mpfr
-    #install_mpc
-    #install_attr
-    #install_acl
-    #install_libcap
-    #install_libxcrypt
-    #install_shadow
-    #install_gcc
-    #install_ncurses
-    #install_sed
-    #install_psmisc
-    #install_gettext
-    #install_bison
-    #install_grep
-    #install_bash
-    #install_libtool
-    #install_gdbm
-    #install_gperf
-    #install_expat
-    #install_inetutils
-    #install_less
-    #install_perl
-    #install_xml_parser
-    #install_intltool
-    #install_autoconf
-    #install_automake
-    #install_openssl
-    #install_kmod
-    #install_libelf_from_elfutils
-    #install_libffi
-    #install_python
-    #install_flitcore
-    #install_whell
-    #install_setuptools
-    #install_ninja
-    #install_meson
-    #install_coreutils
-    #install_check
-    #install_diffutils
-    #install_gawk
-    #install_findutils
-    #install_groff
-    #install_grub
-    #install_gzip
-    #install_iproute2
-    #install_kbd
-    #install_libpipeline
-    #install_make
-    #install_patch
-    #intall_tar
+    #package_management
+    install_man_pages
+    install_iana_etc
+    install_glibc
+    install_zlib
+    install_bzip
+    install_xz
+    install_lz4
+    install_zstd
+    install_file
+    install_readline
+    install_m4
+    install_bc
+    install_flex
+    install_tcl
+    install_spect
+    install_dejaGNU
+    install_pkgconf
+    install_binutils
+    install_gmp
+    install_mpfr
+    install_mpc
+    install_attr
+    install_acl
+    install_libcap
+    install_libxcrypt
+    install_shadow
+    install_gcc
+    install_ncurses
+    install_sed
+    install_psmisc
+    install_gettext
+    install_bison
+    install_grep
+    install_bash
+    install_libtool
+    install_gdbm
+    install_gperf
+    install_expat
+    install_inetutils
+    install_less
+    install_perl
+    install_xml_parser
+    install_intltool
+    install_autoconf
+    install_automake
+    install_openssl
+    install_kmod
+    install_libelf_from_elfutils
+    install_libffi
+    install_python
+    install_flitcore
+    install_whell
+    install_setuptools
+    install_ninja
+    install_meson
+    install_coreutils
+    install_check
+    install_diffutils
+    install_gawk
+    install_findutils
+    install_groff
+    install_grub
+    install_gzip
+    install_iproute2
+    install_kbd
+    install_libpipeline
+    install_make
+    install_patch
+    intall_tar
     install_texinfo
     install_vim
+    install_markupsafe
+    install_jinja2
+    install_udev_from_systemd
+    install_mandb
+    install_procps_ng
+    install_util_linux
+    install_e2fsprogs
+    install_sysklogd
+    install_sysvinit   
+}
 
-    
+stripping() {
+    save_usrlib="$(cd /usr/lib; ls ld-linux*[^g])
+                libc.so.6
+                libthread_db.so.1
+                libquadmath.so.0.0.0
+                libstdc++.so.6.0.33
+                libitm.so.1.0.0
+                libatomic.so.1.2.0"
 
+    cd /usr/lib
 
-    
-    }
+    for LIB in $save_usrlib; do
+        objcopy --only-keep-debug --compress-debug-sections=zlib $LIB $LIB.dbg
+        cp $LIB /tmp/$LIB
+        strip --strip-unneeded /tmp/$LIB
+        objcopy --add-gnu-debuglink=$LIB.dbg /tmp/$LIB
+        install -vm755 /tmp/$LIB /usr/lib
+        rm /tmp/$LIB
+    done
+
+    online_usrbin="bash find strip"
+    online_usrlib="libbfd-2.43.1.so
+                libsframe.so.1.0.0
+                libhistory.so.8.2
+                libncursesw.so.6.5
+                libm.so.6
+                libreadline.so.8.2
+                libz.so.1.3.1
+                libzstd.so.1.5.6
+                $(cd /usr/lib; find libnss*.so* -type f)"
+
+    for BIN in $online_usrbin; do
+        cp /usr/bin/$BIN /tmp/$BIN
+        strip --strip-unneeded /tmp/$BIN
+        install -vm755 /tmp/$BIN /usr/bin
+        rm /tmp/$BIN
+    done
+
+    for LIB in $online_usrlib; do
+        cp /usr/lib/$LIB /tmp/$LIB
+        strip --strip-unneeded /tmp/$LIB
+        install -vm755 /tmp/$LIB /usr/lib
+        rm /tmp/$LIB
+    done
+
+    for i in $(find /usr/lib -type f -name \*.so* ! -name \*dbg) \
+            $(find /usr/lib -type f -name \*.a)                 \
+            $(find /usr/{bin,sbin,libexec} -type f); do
+        case "$online_usrbin $online_usrlib $save_usrlib" in
+            *$(basename $i)* )
+                ;;
+            * ) strip --strip-unneeded $i
+                ;;
+        esac
+    done
+
+    unset BIN LIB save_usrlib online_usrbin online_usrlib
+}
 
 # Función para continuar con la construcción del sistema base
 builddist() {
@@ -2041,7 +2310,9 @@ builddist() {
     #build_util_linux
     #cleaning_and_backup
 
-    installing_basic_system    
+    #installing_basic_system
+    stripping
+
 
 }
 
